a41eb8bd08e18b0a21bbc6d6dfd04af9a1069586
luigicaiaffa <luigicaiaffa8@gmail.com>
2024-12-17 13:04:27 +0100

    add header css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,13 +19,26 @@ export default function Header() {
   }
 
   return (
-    <header>
-      <h1>Boolflix</h1>
-      <div>
-        <form onSubmit={handleFormSubmit}>
-          <input type="text" value={inputValue} onChange={handleInputChange} />
-          <button>Cerca</button>
-        </form>
+    <header className="bg-dark">
+      <div className="d-flex justify-content-between align-items-center p-3">
+        <h1 className="text-danger fw-bold">BOOLFLIX</h1>
+
+        <div>
+          <form onSubmit={handleFormSubmit}>
+            <div className="input-group">
+              <input
+                className="form-control"
+                type="text"
+                value={inputValue}
+                onChange={handleInputChange}
+                aria-describedby="button-addon"
+              />
+              <button className="btn btn-danger" id="button-addon">
+                Cerca
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </header>
   );
