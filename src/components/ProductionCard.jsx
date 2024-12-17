ac8157edc501307965ab649ab21c257fc891d5ca
luigicaiaffa <luigicaiaffa8@gmail.com>
2024-12-17 12:21:48 +0100

    add lang flag

--- src/components/ProductionCard.jsx
+++ src/components/ProductionCard.jsx
@@ -1,11 +1,38 @@
 export default function ProductionCard({ production }) {
-  return (
-    <ul key={production.id}>
-      <li>Titolo: {production.title}</li>
-      <li>Titolo originale: {production.original_title}</li>
-      <li>Lingua: {production.original_language}</li>
-      <li>Voto: {production.vote_average}</li>
-      <li>Poster: {production.poster_path}</li>
-    </ul>
-  );
+    function langFormatter(lang) {
+      if (lang === "en") return "GB";
+      if (lang === "ja") return "JP";
+      return lang.toUpperCase();
+    }
+
+    return (
+      <ul key={production.id}>
+        <li>
+          <strong>Titolo: </strong>
+          {production.title}
+        </li>
+        <li>
+          <strong>Titolo originale: </strong>
+          {production.original_title}
+        </li>
+        <li>
+          <strong>Lingua: </strong>
+          {production.original_language}
+          <img
+            src={`https://flagsapi.com/${langFormatter(
+              production.original_language
+            )}/flat/64.png`}
+            alt=""
+          />
+        </li>
+        <li>
+          <strong>Voto: </strong>
+          {production.vote_average}
+        </li>
+        <li>
+          <strong>Poster: </strong>
+          {production.poster_path}
+        </li>
+      </ul>
+    );
 }
