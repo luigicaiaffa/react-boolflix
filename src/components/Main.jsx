a011a56b975b19ee3d6f628fbe27a763bd4e36eb
luigicaiaffa <luigicaiaffa8@gmail.com>
2024-12-17 16:49:08 +0100

    add bg

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,13 +10,13 @@ export default function Main() {
     <main>
       <div className="container">
         <section className="mt-5">
-          <h1>Film</h1>
+          <h1 className="badge bg-dark text-danger fs-2">FILM</h1>
 
           <ProductionList productions={movies} />
         </section>
 
         <section className="my-5">
-          <h1>Serie TV</h1>
+          <h1 className="badge bg-dark text-danger fs-2">SERIE TV</h1>
 
           <ProductionList productions={series} />
         </section>
